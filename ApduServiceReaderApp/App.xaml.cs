572957a04b4268f17f028830b4a1735b6ba93ca8
Alexander Minaev <mobstel@yandex.ru>
2019-10-17 16:08:23 +0300

    -

--- ApduServiceReaderApp/App.xaml.cs
+++ ApduServiceReaderApp/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace ApduServiceReaderApp
@@ -8,26 +9,10 @@ namespace ApduServiceReaderApp
         public App()
         {
             InitializeComponent();
-
             MainPage = new MainPage();
         }
 
         public static async Task DisplayAlertAsync(string msg) =>
             await Device.InvokeOnMainThreadAsync(async () => await Current.MainPage.DisplayAlert("message from service", msg, "ok"));
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
     }
 }
